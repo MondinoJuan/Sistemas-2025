5c9ed83332f2d33c47221ed75b01617afd51b734
MondinoJuan <juancm.2000@hotmail.com>
2025-05-20 18:28:45 -0300

    videoOCV_Soporte

--- SGDPV/Ejercicios/OpenCV/OpenCV_Video.py
+++ SGDPV/Ejercicios/OpenCV/OpenCV_Video.py
@@ -2,12 +2,31 @@ import cv2
 import datetime
 
 cap = cv2.VideoCapture(0)
+salida = cv2.VideoWriter("video.avi", cv2.VideoWriter_fourcc(*"XVID"), 20.0, (640, 480))
 cont = 0
 
-while True:
-    _, frame = cap.read()
+while (cap.isOpened()):
+    ret, frame = cap.read()
 
-    cv2.imshow("RGB", frame)
+    #cv2.imshow("RGB", frame)
+
+    if ret == True:
+
+        hora_actual = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        posicion = (10, 30)
+        fuente = cv2.FONT_HERSHEY_SIMPLEX
+        escala_fuente = 0.7
+        color_texto = (255, 255, 255)
+        grosor_texto = 2
+        cv2.putText(frame, hora_actual, posicion, fuente, escala_fuente, color_texto, grosor_texto)
+
+
+        cv2.imshow("video", frame)
+        salida.write(frame)
+        if cv2.waitKey(1) & 0xFF == ord('p'):
+            break
+    else:
+        break
 
     if cv2.waitKey(1) & 0xFF == ord('s'):
         print("Guardando imagen...")
@@ -29,4 +48,5 @@ while True:
         break
 
 cap.release()
+salida.release()
 cv2.destroyAllWindows()
